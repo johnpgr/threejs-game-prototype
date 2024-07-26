b0dc321b43f0848048974146a476bf73f46f59e3
johnpgr <jp.greidinger@hotmail.com>
2024-07-26 15:51:39 -0300

    add typia

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+// vite.config.ts
+import UnpluginTypia from "@ryoppippi/unplugin-typia/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [UnpluginTypia()],
+});
